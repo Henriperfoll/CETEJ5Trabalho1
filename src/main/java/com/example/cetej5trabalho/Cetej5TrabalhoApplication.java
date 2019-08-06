16305f5d88879ab80b98b4947e1bc44ce1d084aa
Henrique Perfoll Neto <hperfoll@gmail.com>
2019-08-06 13:52:42 -0300

    first commit

---
+++ src/main/java/com/example/cetej5trabalho/Cetej5TrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cetej5trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cetej5TrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cetej5TrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cetej5trabalho/Cetej5TrabalhoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.cetej5trabalho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cetej5TrabalhoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
